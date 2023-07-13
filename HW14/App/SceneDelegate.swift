5cd2b97d5d295af1e746e81251a7281d5561e8e2
elzel <elzel666@gmail.com>
2023-07-13 23:14:56 +0300

    organized_folders

---
+++ HW14/App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  HW14
+//
+//  Created by Helena on 13.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+    }
+}
+
